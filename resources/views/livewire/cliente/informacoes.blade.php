e83c1f0f65ebbdd761636ec086c1d0e8c2f81807
Miguel Ribeiro <miguelmigasribeiro@gmail.com>
2024-01-06 16:41:31 +0000

    feature-3: Done3

--- resources/views/livewire/cliente/informacoes.blade.php
+++ resources/views/livewire/cliente/informacoes.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="px-12 py-5 bg-white overflow-hidden shadow-sm sm:rounded-lg dark:bg-gray-800">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="px-12 py-5 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
 @if ($estEdit == false)
 
                 <x-primary-button wire:click="edit">{{ __('Editar') }}</x-primary-button>
-                <table class="mt-3 w-full table-fixed border border-slate-700 border-spacing-2 dark:text-gray-100 dark:border-gray-500">
+                <table class="w-full mt-3 border table-fixed border-slate-700 border-spacing-2 dark:text-gray-100 dark:border-gray-500">
                     <thead>
                     <tr>
                         <th class="px-3 py-2">Data de Nascimento</th>
@@ -31,17 +31,17 @@
 </h2>
 <form wire:submit="update">
     @csrf
-    <div class="mb-5 px-3">
+    <div class="px-3 mb-5">
         <x-input-label for="telefone" :value="__('Telefone')" />
-        <x-text-input wire:model="telefone" id="telefone" name="telefone" type="text" class="mt-1 block w-full" :value="$cliente->pluck('telefone')->first()" />
+        <x-text-input wire:model="telefone" id="telefone" name="telefone" type="text" class="block w-full mt-1" :value="$cliente->pluck('telefone')->first()" />
         <x-input-error class="mt-2" :messages="$errors->get('telefone')" />
     </div>
     <div class="px-3">
         <x-input-label for="Morada" :value="__('Morada')" />
-        <x-text-input wire:model="Morada" id="Morada" name="Morada" type="text" class="mt-1 block w-full" :value="$cliente->pluck('Morada')->first()" />
+        <x-text-input wire:model="Morada" id="Morada" name="Morada" type="text" class="block w-full mt-1" :value="$cliente->pluck('Morada')->first()" />
         <x-input-error class="mt-2" :messages="$errors->get('Morada')" />
     </div>
-    <div class="mt-5 px-3">
+    <div class="px-3 mt-5">
         <x-primary-button type="submit">{{__('Alterar')}}</x-primary-button>
     
 </form>
