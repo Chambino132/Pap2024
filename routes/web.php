e6ae0549438f082cce2cb0544e661ce577669703
Miguel Ribeiro <miguelmigasribeiro@gmail.com>
2024-01-06 16:50:25 +0000

    feature-3: DONE

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/clientes', [ViewController::class, 'cliente'])->name('clientes');
+    Route::get('/informacoes', function () {
+        return view('cliente.index');
+    })->name('clieInform');
 });
 
 require __DIR__ . '/auth.php';
